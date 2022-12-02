d6a2cd3b4cd5474b5f621fc7aba96784e7e8812e
ddd-fx <d.khoroshkin@gmail.com>
2022-12-02 23:38:56 +0600

    update rootDir

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
     },
-    rootDir: './_tests',
+    rootDir: './src/_tests',
     transform: {
         '^.+\\.(ts|tsx)?$': 'ts-jest',
         '.+\\.(css|styl|less|sass|scss|png|jpg|webp|ttf|woff|woff2)$': 'jest-transform-stub',
